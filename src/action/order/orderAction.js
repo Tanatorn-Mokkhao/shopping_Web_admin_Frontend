749a0197bddf7ff742e63e5c07c5be4e777c5122
Tanatorn Mokkhao <tanatorn.mokkhao@gmail.com>
2022-04-13 12:00:43 +0700

    resume for meb

--- src/action/order/orderAction.js
+++ src/action/order/orderAction.js
@@ -1,19 +1,19 @@
-// import axios from '../../helper/axios'
+import axios from '../../helper/axios'
 import { orderType } from './orderType'
-import axios from "axios"
+// import axios from "axios"
 import { Config_ls } from '../../utils/config'
 
 export const GetOrder = (page,orderBy,search,state) => { 
     return async (dispatch) => { 
         try {
             dispatch({ type: orderType.GET_ORDER_REQUEST })
-            const token = window.localStorage.getItem(Config_ls.userToken)
-            const res = await axios.get(`http://localhost:3003/orders?limit=10&page=${page}&orderBy=${orderBy}&search=${search}&state=${state}`, {
-                headers: {
-                    'Authorization': token ? `Bearer ${token}` : ''
-                }
-            })
-            // const res = await axios.get(`/orders?limit=10&page=${page}&orderBy=${orderBy}&search=${search}&state=${state}`)
+            // const token = window.localStorage.getItem(Config_ls.userToken)
+            // const res = await axios.get(`http://localhost:3003/orders?limit=10&page=${page}&orderBy=${orderBy}&search=${search}&state=${state}`, {
+            //     headers: {
+            //         'Authorization': token ? `Bearer ${token}` : ''
+            //     }
+            // })
+            const res = await axios.get(`/orders?limit=10&page=${page}&orderBy=${orderBy}&search=${search}&state=${state}`)
             if (res.status === 200) { 
                 const { data, filter } = res.data
                 dispatch({
@@ -63,13 +63,13 @@ export const PatchCancleOrderAccept = (payload) => {
     return async (dispatch) => { 
         try {
             dispatch({ type: orderType.PATACH_CANCLE_ORDER_REQUEST })
-            const token = window.localStorage.getItem(Config_ls.userToken)
-            const res = await axios.patch(`http://localhost:3003/orders/cancle-order-accept`, payload , {
-                headers: {
-                    'Authorization': token ? `Bearer ${token}` : ''
-                }
-            })
-            // const res = await axios.get(`/orders?limit=10&page=${page}&orderBy=${orderBy}&search=${search}&state=${state}`)
+            // const token = window.localStorage.getItem(Config_ls.userToken)
+            // const res = await axios.patch(`http://localhost:3003/orders/cancle-order-accept`, payload , {
+            //     headers: {
+            //         'Authorization': token ? `Bearer ${token}` : ''
+            //     }
+            // })
+            const res = await axios.patch('/orders/cancle-order-accept', payload)
             if (res.status === 200) { 
                 const { search } = window.location
                 const page = new URLSearchParams(search).get("page")
